cf400f91ce535c3eb7500e2cad027c071b33b6a4
miguel <miguel@teste.com.br>
2020-10-26 15:39:03 -0300

    primeiros passo com JS

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var frutas=[{nome:"Maça", cor:"vermelha"},{nome:"peira", cor:"amarela"}];
+console.log(frutas.nome);
+
+
+//var fruta = {nome:"Maça", cor:"vermelha"}
+//console.log (fruta)
+//console.log(fruta.nome)
+
+//var lista = ["maça","peira","laranja"];
+//lista.push("uva");// adiciona elemento a lista 
+//lista.pop();//remove elemento da lista
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(""));
+
+//var nome = "Miguel Awad";
+//var idade = 24;
+//var idade2 = 1;
+//var frase = "Japão é o melhor time do mundo!"
+//alert(nome + " tem " + idade+" anos");
+//alert (idade+idade2);
+//console.log(nome);
+//console.log(idade+idade2);
+//console.log(frase.toUpperCase())
+
